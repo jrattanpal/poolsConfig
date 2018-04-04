b1382c61ad8cf351d598fe364ed401b893256cf9
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-04-04 15:01:44 -0500

    include dev option

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const pools = require('lib/pools');
+const poolsDev = require('lib/poolsDev');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/pools', (req, res, next) => res.send(pools))
+  .get('/pools-dev', (req, res, next) => res.send(poolsDev))
   .get('/', (req, res) => res.render('pages/index'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
