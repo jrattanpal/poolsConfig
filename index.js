53f9e3179602f979f7727cefb2062620f8f61e1f
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-04-04 14:58:33 -0500

    initial setup + 5 trial

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+const pools = require('lib/pools');
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/pools', (req, res) => res.send(pools))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
